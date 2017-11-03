c6f8dac159bf76d2a37b8e7b03282e9dc26caada
bitao1 <bitao1@lenovo.com>
2017-11-03 17:26:26 +0800

    bitao 2017/11/3

--- pages/findbuddydetail/findbuddydetail.js
+++ pages/findbuddydetail/findbuddydetail.js
@@ -23,5 +23,62 @@ Page({
           })
         }
       })
+    },
+
+    btnDelete: function(e){
+      wx.showLoading({
+        title: 'Deleting',
+      })
+      var links = this.data.array._links
+      wx.request({
+        url: links.delete.href + "?access-token=" + this.data.access_token,
+        data: Util.json2Form({
+
+        })
+        ,
+        header: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        method: "DELETE",
+        complete: function (res1) {
+          if (res1 == null || res1.statusCode != 204) {
+            console.error(res1.statusCode);
+            setTimeout(function () {
+              wx.hideLoading()
+            }, 2000)
+            wx.showToast({
+              title: 'save fail',
+              icon: 'fail',
+              duration: 2000
+            })
+            setTimeout(function () {
+              wx.hideToast()
+            }, 2000)
+            return;
+          }
+          console.log(res1)
+          setTimeout(function () {
+            wx.hideLoading()
+          }, 2000)
+          wx.showToast({
+            title: 'Delete Success',
+            icon: 'success',
+            duration: 2000
+          })
+          setTimeout(function () {
+            wx.hideToast()
+          }, 2000)
+          var pages = getCurrentPages();
+          if (pages.length > 1) {
+            //上一个页面实例对象
+            var prePage = pages[pages.length - 2];
+            //关键在这里
+            prePage.onLoad()
+          }
+          wx.navigateBack({
+            delta: 1
+          })
+        }
+      })
     }
 });
--- pages/findbuddysearch/findbuddysearch.js
+++ pages/findbuddysearch/findbuddysearch.js
@@ -25,6 +25,64 @@ Page({
       }
     })
   },
+
+  btnSave: function(e){
+    wx.showLoading({
+      title: 'Saveing',
+    })
+    var links = this.data.array._links
+    wx.request({
+      url: links.participate.href + "?access-token=" + this.data.access_token,
+      data: Util.json2Form({
+
+      })
+      ,
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      method: "POST",
+      complete: function (res1) {
+        if (res1 == null || res1.statusCode != 201) {
+          console.error(res1.statusCode);
+          setTimeout(function () {
+            wx.hideLoading()
+          }, 2000)
+          wx.showToast({
+            title: 'save fail',
+            icon: 'fail',
+            duration: 2000
+          })
+          setTimeout(function () {
+            wx.hideToast()
+          }, 2000)
+          return;
+        }
+        console.log(res1)
+        setTimeout(function () {
+          wx.hideLoading()
+        }, 2000)
+        wx.showToast({
+          title: 'Join Success',
+          icon: 'success',
+          duration: 2000
+        })
+        setTimeout(function () {
+          wx.hideToast()
+        }, 2000)
+        var pages = getCurrentPages();
+        if (pages.length > 1) {
+          //上一个页面实例对象
+          var prePage = pages[pages.length - 2];
+          //关键在这里
+          prePage.onLoad()
+        }
+        wx.navigateBack({
+          delta: 1
+        })
+      }
+    })
+  },
+
   onReady:function(){
     // 页面渲染完成
   },
